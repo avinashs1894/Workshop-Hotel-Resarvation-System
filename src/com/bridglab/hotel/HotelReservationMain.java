1cc0bd92d8c8cf169d77894bf76acc43b0f65569
unknown <avinash.sankajji94@gmai.com>
2021-10-26 22:15:02 +0530

    Added:Use Case 2

--- src/com/bridglab/hotel/HotelReservationMain.java
+++ src/com/bridglab/hotel/HotelReservationMain.java
@@ -1,10 +1,13 @@
 package com.bridglab.hotel;
 
+
+import java.util.*;
+
 public class HotelReservationMain {
     public static void main(String[] args) {
 
         HotelReservation hotelReservation = new HotelReservation();
         hotelReservation.viewHotels();
-
+        hotelReservation.getMinPriceHotel();
     }
 }
